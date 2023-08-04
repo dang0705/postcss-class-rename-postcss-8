dcc4bf7a75f55f9fa259e41f5a07d2e55ef1270f
yujie <jie.yu.3@volvocars.com>
2023-08-04 19:02:23 +0800

    feat(init):

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const postcss = require("postcss");
-
-module.exports = postcss.plugin("postcss-class-rename", options => {
+module.exports =  (options) => {
   const isObject = typeof options === "object" && options instanceof Object;
 
-  if (!isObject) {
-    return;
-  }
+  if (!isObject) return;
 
-  return root => {
-    const replaceClass = (pattern, replacement) => {
-      if (typeof pattern !== "string") {
-        return;
-      }
-
-      root.walk(rule => {
-        const regex = new RegExp(pattern, "gi");
-
-        if (rule.type === "rule") {
-          rule.selector = rule.selector.replace(regex, replacement);
-        } else if (rule.type === "atrule") {
-          rule.params = rule.params.replace(regex, replacement);
+  return {
+    postcssPlugin:"postcss-class-rename-pc-8",
+    Once (root){
+      const replaceClass = (pattern, replacement) => {
+        if (typeof pattern !== "string") {
+          return;
         }
-      });
-    };
 
-    // Replace all defined pattern and replacement
-    Object.keys(options).forEach(function(p) {
-      replaceClass(p, options[p]);
-    });
-  };
-});
+        root.walk(rule => {
+          const regex = new RegExp(pattern, "gi");
+
+          if (rule.type === "rule") {
+            rule.selector = rule.selector.replace(regex, replacement);
+          } else if (rule.type === "atrule") {
+            rule.params = rule.params.replace(regex, replacement);
+          }
+        });
+      };
+
+      // Replace all defined pattern and replacement
+      Object.keys(options).forEach(function(p) {
+        replaceClass(p, options[p]);
+      });
+    }
+  }
+};
+module.exports.postcss=true
